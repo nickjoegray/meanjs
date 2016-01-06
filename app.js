67eafc79db0b822f0c6b63375637492288a184b5
Nick Gray <nick.gray@rci.rogers.ca>
2016-01-06 16:17:53 -0500

    Add page layout and posts factory to create new posts with upvoting

---
+++ app.js
@@ -0,0 +1,31 @@
+var app = angular.module('upvoter', []);
+
+app.controller('MainCtrl', [
+'$scope',
+'posts',
+
+function($scope, posts){
+	$scope.posts = posts.posts;
+
+  $scope.addPost = function() {
+  	if(!$scope.title || $scope.title === '') { return; }
+		$scope.posts.push({
+			title: $scope.title, 
+			upvotes: 0,
+			link: $scope.link
+		});
+		$scope.title = '';	
+		$scope.link = '';
+  };
+
+  $scope.incrementUpvotes = function (post) {
+  	post.upvotes += 1;
+  }
+}])
+
+app.factory('posts', [function() {
+	var o = {
+		posts: []
+	};
+	return o;
+}]);
